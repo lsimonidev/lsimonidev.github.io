56f655b328fee0f1c3d4dc0596a06f483338cded
Laura Simoni <laura.simoni@hostelworld.com>
2023-08-09 17:35:50 +0100

    starting Home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import Timeline from './components/Timeline.vue';
+
 </script>
 
-<template>
-  <div class="section">
-    <div class="container">
-      <Timeline />
-    </div>
-  </div>
-</template>
+<template></template>
 
 <style scoped></style>
